b80366ba15ffe892f45a7b00b2a86630aece1ce8
edgar begay <dixonbegay@gmail.com>
2019-05-09 07:58:29 -0700

    Added some more files to show more files as part of example

---
+++ src/components/github-link.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function GithubLink() {
+  return (
+    <a href="https://github.com/shadez95/nextjs-typescript-boilerplate">
+      Github
+    </a>
+  );
+}
---
+++ src/components/help.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { GithubLink } from './github-link';
+
+export function Help() {
+  return (
+    <p>
+      Please see <GithubLink /> page for more help and information
+    </p>
+  );
+}
